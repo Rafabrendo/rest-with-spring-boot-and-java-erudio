a988a134e85a314f0307d94eba2061f6482ffbaf
Rafael Brendo <rafalive122@hotmail.com>
2023-02-22 10:36:57 -0300

    finalizing migrations with the FlyWay tool

---
+++ 10_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE IF NOT EXISTS `person` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `fist_name` varchar(80) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
